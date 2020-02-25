d48eec8e32441df26550be863a647e7532cf5444
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-25 04:01:51 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+#require_relative './lib/artist.rb'
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+   def artist_name
+    @artist ? @artist.name : nil
+  end
+  
+end
+  
+  
