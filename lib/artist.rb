a4b3a77f1ff8c1406cca130a38b91526116ba432
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-25 02:19:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,19 @@
-require 'relative'
+require_relative './lib/song.rb'
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @all
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+end
